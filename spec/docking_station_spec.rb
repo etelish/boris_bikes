6598a8bf42dac69bcd53b588c15d1fd33538d889
Daniel Segall <segalld@gmail.com>
2019-08-21 15:00:08 +0100

    Fill in missing test in bike_spec for working method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require './lib/docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to 'release_bike' }
+  it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
     expect(subject.release_bike.working?).to eq true
