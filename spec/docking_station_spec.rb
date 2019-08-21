b4f7c34a5846eb2ff35c66671822f87961617145
Daniel Segall <segalld@gmail.com>
2019-08-21 14:51:13 +0100

    Add test for working bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
-require 'docking_station'
+require './lib/docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
- end
+  it { is_expected.to respond_to 'release_bike' }
+
+  it 'releases working bikes' do
+    expect(subject.release_bike.working?).to eq true
+  end
+end
